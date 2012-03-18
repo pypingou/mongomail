fcb43c4025eac2214500938abaeedf5d23be7687
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-03-18 09:29:26 +0100

    Let's make it working over several lists

--- get_mbox.py
+++ get_mbox.py
@@ -5,23 +5,27 @@ import urlgrabber
 import gzip
 from multiprocessing import Pool
 
-
-years = (2010, 2011, 2012, 2009, 2008, 2007, 2006, 2005, 2004)
-months = ('January', 'February', 'March', 'April', 'May', 'June', 'July',
-    'August', 'September', 'October', 'November', 'December')
+lists = ['devel']
+years = [2010, 2011, 2012, 2009, 2008, 2007, 2006, 2005, 2004]
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 'November', 'December']
 
 
 def archive_downloader(i):
-    basename = "{0}-{1}.txt.gz".format(*i)
-    filename = "http://lists.fedoraproject.org/pipermail/devel/{0}".format(basename)
+    list_name = i[0]
+    year = i[1]
+    month = i[2]
+    if not list_name or not year or not month:
+        return
+    basename = "{0}-{1}.txt.gz".format(year, month)
+    filename = "http://lists.fedoraproject.org/pipermail/{0}/{1}".format(
+        list_name, basename)
     try:
         urlgrabber.urlgrab(filename)
-        year = basename.split('-')[0]
-        month = basename.split('-')[1].split('.')[0]
         pos = str(months.index(month) + 1)
         if len(pos) == 1:
-            pos = '0%s' % pos
-        newname = '%s-%s-%s.txt' % (year, pos, month)
+            pos = '0{0}'.format(pos)
+        newname = '{0}-{1}-{2}-{3}.txt'.format(list_name, year, pos, month)
         with open(newname, "w") as f:
            f.write(gzip.open(basename).read())
         print "== {0} downloaded ==".format(filename)
@@ -30,4 +34,4 @@ def archive_downloader(i):
 
 if __name__ == "__main__":
     p = Pool(5)
-    p.map(archive_downloader, itertools.product(years, months))
+    p.map(archive_downloader, itertools.product(lists, years, months))
