d4719731c026cf2ff598214e8b9ee73fde6c93e3
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-03-22 15:12:05 +0100

    Let's go a little more back

--- get_mbox.py
+++ get_mbox.py
@@ -9,7 +9,7 @@ from multiprocessing import Pool
 #lists = ['packaging']
 #lists = ['fr-users']
 #lists = ['devel', 'packaging', 'fr-users']
-years = [2010, 2011, 2012, 2009, 2008, 2007, 2006, 2005, 2004]
+years = [2010, 2011, 2012, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002]
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
     'August', 'September', 'October', 'November', 'December']
 
