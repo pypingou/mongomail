d921ad9028e94f23f7dcd99d7b832a38505796e8
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-03-31 18:30:24 +0200

    Remove all the dashes within the attributes of a message

--- to_mongo.py
+++ to_mongo.py
@@ -40,14 +40,15 @@ def to_mongo(mbfile, database):
         ## TODO: We need to catch-up Subjects/From which are of a specific
         ## encoding.
         for it in message.keys():
-            infos[it] = message[it]
+            it2 = it.replace('-', '')
+            infos[it2] = message[it]
         keys = infos.keys()
         ## There seem to be a problem to parse some messages
         if not keys:
             print '  Failed: %s keys: "%s"' % (mbfile, keys)
             #print message
             continue
-        infos['Message-ID'] = infos['Message-ID'].replace('<', ''
+        infos['MessageID'] = infos['MessageID'].replace('<', ''
             ).replace('>', '')
         if 'From' in infos:
             regex = '(.*)\((.*)\)'
@@ -58,8 +59,7 @@ def to_mongo(mbfile, database):
                 email = email.replace(' at ', '@')
                 infos['Email'] = email
         try:
-            if '--assume-unique' in sys.argv or \
-                db.mails.find({'Message-ID': infos['Message-ID']}).count() == 0:
+            if db.mails.find({'MessageID': infos['MessageID']}).count() == 0:
                 infos['Date'] = convert_date(infos['Date'])
                 infos['Content'] = message.get_payload()
                 try:
@@ -68,23 +68,24 @@ def to_mongo(mbfile, database):
                     ## TODO: Do something about this encoding issue
                     raise InvalidStringData('Email has invalid content')
                 thread_id = 0
-                db.mails.create_index('Message-ID')
-                db.mails.ensure_index('Message-ID')
+                db.mails.create_index('MessageID')
+                db.mails.ensure_index('MessageID')
                 db.mails.create_index('ThreadID')
                 db.mails.ensure_index('ThreadID')
-                if not 'References' in infos and not 'In-Reply-To' in infos:
-                    infos['ThreadID'] = b32encode(sha1(infos['Message-ID']).digest())
+                if not 'References' in infos and not 'InReplyTo' in infos:
+                    infos['ThreadID'] = b32encode(sha1(infos['MessageID']).digest())
                 else:
                     ref = None
-                    if 'In-Reply-To' in infos:
-                        ref= infos['In-Reply-To']
+                    if 'References' in infos:
+                        ref= infos['References'].split()[0].strip()
                     else:
-                        ref= infos['References'].split('\n')[0].strip()
-                    res = db.mails.find_one({'Message-ID': ref})
+                        ref= infos['InReplyTo']
+                    ref = ref.replace('<', '').replace('>', '')
+                    res = db.mails.find_one({'MessageID': ref})
                     if res and 'ThreadID' in res:
                         infos['ThreadID'] = res['ThreadID']
                     else:
-                        infos['ThreadID'] = b32encode(sha1(infos['Message-ID']).digest())
+                        infos['ThreadID'] = b32encode(sha1(infos['MessageID']).digest())
                 infos['Category'] = 'Question'
                 if 'agenda' in infos['Subject'].lower():
                     infos['Category'] = 'Agenda'
@@ -115,7 +116,7 @@ def get_document_size(database):
 
 
 if __name__ == '__main__':
-    sys.argv.extend(['devel', 'lists/devel-2012-03-March.txt'])
+    #sys.argv.extend(['devel', 'lists/devel-2012-03-March.txt'])
     if len(sys.argv) < 2 or '-h' in sys.argv or '--help' in sys.argv:
         print '''USAGE:
 python mbox_to_mongo.py db_name mbox_file [mbox_file]'''
